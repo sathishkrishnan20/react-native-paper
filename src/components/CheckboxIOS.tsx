f06e1f997e9a0a6826a3119d33a50366d48eb2b9
sathishkrishnan20 <sathishkrish20@gmail.com>
2020-04-15 14:00:20 +0530

    Update CheckboxIOS.tsx

--- src/components/CheckboxIOS.tsx
+++ src/components/CheckboxIOS.tsx
@@ -1,3 +1,4 @@
+
 import * as React from 'react';
 import { StyleSheet, View, I18nManager } from 'react-native';
 import color from 'color';
@@ -50,7 +51,7 @@ class CheckboxIOS extends React.Component<Props> {
   static displayName = 'Checkbox.IOS';
 
   render() {
-    const { status, disabled, onPress, customContainerStyle, customViewStyle,  theme, ...rest } = this.props;
+    const { status, disabled, onPress, customContainerStyle, customViewStyle, theme, ...rest } = this.props;
     const checked = status === 'checked';
     const indeterminate = status === 'indeterminate';
 
@@ -73,58 +74,40 @@ class CheckboxIOS extends React.Component<Props> {
     }
 
     const icon = indeterminate ? 'minus' : 'check';
-    
+
     return (
       <TouchableRipple
-      {...rest}
-      borderless
-      rippleColor={rippleColor}
-      onPress={
-        disabled
-          ? undefined
-          : () => {
-              handlePress({
-                onPress,
-                value,
-                onValueChange: context?.onValueChange,
-              });
-            }
-      }
-      accessibilityTraits={disabled ? ['button', 'disabled'] : 'button'}
-      accessibilityComponentType={
-        checked ? 'radiobutton_checked' : 'radiobutton_unchecked'
-      }
-      accessibilityRole="button"
-      accessibilityStates={disabled ? ['disabled'] : []}
-      accessibilityLiveRegion="polite"
-      style={customContainerStyle ? customContainerStyle : styles.container}
-    >
-      <View style={[{ opacity: indeterminate || checked ? 1 : 0 }, customViewStyle ? customViewStyle : styles.viewStyle]}>
-        <MaterialCommunityIcon
-          allowFontScaling={false}
-          name="check"
-          size={17.5}
-          color={"#fff"}
-          direction={I18nManager.isRTL ? 'rtl' : 'ltr'}
-        />
-      </View>
-    </TouchableRipple>
-
+        {...rest}
+        borderless
+        rippleColor={rippleColor}
+        onPress={onPress}
+        disabled={disabled}
+        accessibilityTraits={disabled ? ['button', 'disabled'] : 'button'}
+        accessibilityComponentType="button"
+        accessibilityRole="button"
+        accessibilityStates={disabled ? ['disabled'] : []}
+        accessibilityLiveRegion="polite"
+        style={customContainerStyle ? customContainerStyle : styles.container}
+      >
+         <View style={[{ opacity: indeterminate || checked ? 1 : 0 }, customViewStyle ? customViewStyle : styles.viewStyle]}>
+          <MaterialCommunityIcon
+           allowFontScaling={false}
+           name="check"
+           size={17.5}
+           color={"#fff"}
+           direction={I18nManager.isRTL ? 'rtl' : 'ltr'}
+          />
+        </View>
+      </TouchableRipple>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    borderRadius: 2,
-    height:16, 
-    borderColor:'#000',
-    borderWidth:0.5,
+    borderRadius: 18,
+    padding: 6,
   },
-  viewStyle:{
-    marginTop:-2,
-    backgroundColor:"#775DA3"
-   }
 });
 
 export default withTheme(CheckboxIOS);
