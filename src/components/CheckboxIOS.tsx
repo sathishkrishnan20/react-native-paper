14b62746fc889b4e2478cd5e3186e0a5224c7f00
sathishkrishnan20 <sathishkrish20@gmail.com>
2020-04-15 14:01:12 +0530

    Update CheckboxIOS.tsx

--- src/components/CheckboxIOS.tsx
+++ src/components/CheckboxIOS.tsx
@@ -105,9 +105,15 @@ class CheckboxIOS extends React.Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    borderRadius: 18,
-    padding: 6,
+    borderRadius: 2,
+    height:16, 
+    borderColor:'#000',
+    borderWidth:0.5,
   },
+  viewStyle:{
+    marginTop:-2,
+    backgroundColor:"#775DA3"
+  }
 });
 
 export default withTheme(CheckboxIOS);
